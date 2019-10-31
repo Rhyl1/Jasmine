fff3729d76030ae73f66e986dfe9616b4ced1c00
Rhy <yhr@xs4all.nl>
2019-10-31 07:04:31 +0000

    added content to calcSpec and calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,22 @@
-function addition (numOne, NumTwo) {
-    if (typeof(numOne) == "number" && typeof(NumTwo)=="number") {
-        return numOne + NumTwo;
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number)=="number") {
+        this.value += number;
     } else {
         alert("Error!");
     }
 }
+
+
+
+
+// function addition (numOne, NumTwo) {
+//     if (typeof(numOne) == "number" && typeof(NumTwo)=="number") {
+//         return numOne + NumTwo;
+//     } else {
+//         alert("Error!");
+//     }
+// }
